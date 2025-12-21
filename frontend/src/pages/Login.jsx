649e3a012049b40b01b9080af5617e7309f906c4
Sachin Kapoor <sachinkapoor@Sachins-MacBook-Air.local>
2025-12-21 21:39:32 +0530

    update infra

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { login } from "../services/auth";
 export default function Login() {
   return (
     <div>
-      <img src="/images/logo.png" width="150" />
+      <img src="/images/logo.png" width="150" alt="Company Logo" />
       <button onClick={login}>Login with Company Account</button>
     </div>
   );
