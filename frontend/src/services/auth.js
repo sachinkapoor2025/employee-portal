fce79f421a3b3786edd6b45651d8c74e2adace35
Sachin Kapoor <sachinkapoor@Sachins-MacBook-Air.local>
2025-12-23 01:41:56 +0530

    update admin frontend

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -27,9 +27,11 @@ export const handleCallback = async () => {
 
     // Check access
     try {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/access`, {
-        headers: { Authorization: token }
-      });
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/access`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
       const data = await res.json();
 
       if (data.access === 'ADMIN') {
